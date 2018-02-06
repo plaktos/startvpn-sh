9a523830cda73fd4aff7d0e505b928fd9151b255
plaktos <plaktos@protonmail.com>
2018-02-07 01:50:23 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# What is this?
+
+It's a simple bash script for starting an SSL tunnel and an OpenVPN connection
+based on a directory system.
+
+# How can I use this?
+
+You need to acquire .ssl and .ovpn files from your VPN.
+
+Create a folder where you will keep your VPN files
+`mkdir /home/VPN/`
+
+Copy your .ssl and .ovpn files based on this structure.
+
+Each server has it's own folder. Say you have VPN servers in New York, Budapest and Tokyo
+create three folders for them.
+`cd /home/VPN/`
+`mkdir NewYork Budapest Tokyo`
+
+Clone into this repository
+`git clone https://github.com/plaktos/startvpn-sh.git`
+
+Run the script, you need superuser privileges
+`sudo ./startvpn.sh`
+
+Pick a server and press enter.
---
+++ startvpn.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+locationDirs="$(ls -d */)" &&
+    dirsNum=$(echo "$locationDirs" | wc -w) &&
+    locationArray=($locationDirs)
+
+declare stunnel_pid
+declare openvpn_pid
+
+print_dirs(){
+    for((i=0;i<dirsNum;++i)) do
+        printf "\t%d-%s\n" $i ${locationArray[i]%/}
+    done
+}
+
+start_vpn(){
+    cd "$1"
+    sslfile=$(find -type f -name "*.ssl" | head -n 1) 
+    ovpnfile=$(find -type f -name "*.ovpn" | head -n 1)
+
+    if [[ ( -n "$sslfile" ) && ( -n "$ovpnfile" ) ]]; then
+        $(stunnel "$sslfile") &
+        stunnel_pid=$!
+        $(openvpn "$ovpnfile") &
+        openvpn_pid=$!
+        wait $stunnel_pid $openvpn_pid
+    else
+        if [[ ! -n "$sslfile" ]]; then
+            printf "No .ssl file in %s\n" "$1" >&2
+        fi
+        if [[ ! -n "$ovpnfile" ]]; then
+            printf "No .ovpn file in %s\n" "$1" >&2
+        fi
+        exit
+    fi
+}
+
+if [[ "$EUID" > 0 ]]; then
+    echo "Please run as root" >&2
+    exit
+else
+    while true; do
+        echo -e "\nChoose a VPN Server:\n"
+        print_dirs
+        echo -e "\n\tOr input 'q' to exit"
+        read -p "Enter selection > "
+
+        if [[ -n "$REPLY" ]]; then
+            if [[ "$REPLY" == 'q' ]]; then
+                echo "Exiting..."
+                exit
+            elif [[ ("$REPLY" -ge 0) && ("$REPLY" -lt ${#locationArray[@]}) ]]; then
+                chosen=${locationArray[$REPLY]%/}
+                start_vpn $chosen
+                exit
+            fi
+        fi
+    done
+fi
+
+trap exit_signal SIGINT SIGTERM
+exit_signal(){
+    if [[ -n $stunnel_pid ]]; do kill $stunnel_pid SIGINT done
+    if [[ -n $openvpn_pid ]]; do kill $openvpn_pid SIGINT done
+    exit
+}
